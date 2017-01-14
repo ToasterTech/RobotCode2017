51435123e6c902a2f3b466fbe7e14507e83cf5b4
Squidoinkoo <rwoodkat4@gmail.com>
2017-01-13 20:40:48 -0500

    I added the system methods to the drive system base. :D

--- src/org/usfirst/frc/team5332/robot/drive/base/DriveSystemBase.java
+++ src/org/usfirst/frc/team5332/robot/drive/base/DriveSystemBase.java
@@ -3,5 +3,16 @@ package org.usfirst.frc.team5332.robot.drive.base;
 import org.usfirst.frc.team5332.subsystem.Layer;
 
 public abstract class DriveSystemBase implements Layer<DriveHardwareBase>{
+	
+	@Override
+	public abstract void periodicUpdate();
 
+	@Override
+	public abstract void setChild(DriveHardwareBase c);
+	
+	public abstract void setDriveRight(double speed);
+	
+	public abstract void setDriveLeft(double speed);
+	
+	public abstract double distanceForward(double distance);
 }
