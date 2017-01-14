8c69fcfc7f49ba4328a1fc0ad8c2b5e0a4396560
myaccount <myaccount@lawn-143-215-108-224.lawn.gatech.edu>
2017-01-13 20:33:43 -0500

    Created the templates for the templates for the hardware class.

--- src/org/usfirst/frc/team5332/robot/drive/base/DriveHardwareBase.java
+++ src/org/usfirst/frc/team5332/robot/drive/base/DriveHardwareBase.java
@@ -4,4 +4,13 @@ import org.usfirst.frc.team5332.subsystem.BaseLayer;
 
 public abstract class DriveHardwareBase implements BaseLayer{
 
+	public abstract void setDriveRight(double speed);
+	
+	public abstract void setDriveLeft(double speed);
+	
+	public abstract int rightDriveCount();
+	
+	public abstract int leftDriveCount();
+	
+	public abstract void encoderReset();
 }
