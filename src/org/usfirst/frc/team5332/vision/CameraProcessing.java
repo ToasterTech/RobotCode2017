dc45fd46bd3be6f0631e0800fd551868d98ba6b8
J-Dierberger-Laptop <quantum.di@bellsouth.net>
2017-01-17 06:53:12 -0500

    Added CameraProcessing class

--- src/org/usfirst/frc/team5332/vision/CameraProcessing.java
+++ src/org/usfirst/frc/team5332/vision/CameraProcessing.java
@@ -1,7 +1,13 @@
 package org.usfirst.frc.team5332.vision;
 
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
 public class CameraProcessing{
 	
-	
+	public static void processImage(){
+		
+	}
 	
 }
