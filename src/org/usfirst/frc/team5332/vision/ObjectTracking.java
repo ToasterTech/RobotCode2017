fba9474117ab497d1df58123dcf5fe00d739d4d3
Krishan <Krishan@DESKTOP-7TDOBEI>
2017-01-20 20:42:44 -0500

    Theoretically functional contours

--- src/org/usfirst/frc/team5332/vision/ObjectTracking.java
+++ src/org/usfirst/frc/team5332/vision/ObjectTracking.java
@@ -1,7 +1,12 @@
 package org.usfirst.frc.team5332.vision;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc.team5332.util.Constants;
@@ -11,6 +16,8 @@ public class ObjectTracking {
 	private static Mat blurredImage = new Mat();
 	private static Mat hsvImage = new Mat();
 	private static Mat coloredImg = new Mat();
+	private static List<MatOfPoint> contours = new ArrayList<>();
+	private static Mat contouredImg = new Mat();
 	
 	public static Mat filterColor(Mat unedited) {
 		Imgproc.cvtColor(blurredImage, hsvImage, Imgproc.COLOR_BGR2HSV);
@@ -18,9 +25,18 @@ public class ObjectTracking {
 		return coloredImg;
 	}
 		
-	public static void getContour(Mat frame) {
-		Imgproc.blur(frame, blurredImage, new Size(7, 7));
+	public static void getContour(Mat coloredImg) {
+		Imgproc.blur(coloredImg, blurredImage, new Size(7, 7));
+		Imgproc.findContours(coloredImg, contours, contouredImg, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+		MatOfPoint largestBlob = contours.get(0);
 		
+		for (MatOfPoint contour: contours) {
+			if (contour.size().area() >= largestBlob.size().area()) {
+				largestBlob = contour;
+			}
+		}
+		
+		Imgproc.drawContours(contouredImg, contours, -1, new Scalar(250, 0, 0));
 	}
 
 }
