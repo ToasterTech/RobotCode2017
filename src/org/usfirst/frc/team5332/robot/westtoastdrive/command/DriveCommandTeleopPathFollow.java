d4586aa9b7dfdee7f806b387f032aa81c23a8ef4
J-Dierberger-Laptop <quantum.di@bellsouth.net>
2017-01-28 15:40:48 -0500

    Path following code

--- src/org/usfirst/frc/team5332/robot/westtoastdrive/command/DriveCommandTeleopPathFollow.java
+++ src/org/usfirst/frc/team5332/robot/westtoastdrive/command/DriveCommandTeleopPathFollow.java
@@ -21,22 +21,34 @@ public class DriveCommandTeleopPathFollow extends DriveCommandBase{
 	@Override
 	public void periodicUpdate(){
 		// Citation: http://stackoverflow.com/questions/25835510/draw-curved-line-between-two-points
-		double[] coords = path.getNextCoordinate();
 		
-		// Translate our coordinates so that the robot's current position is on the origin
-		coords[0] -= systemLayer.getCurrentGlobalCoords()[0];
-		coords[1] -= systemLayer.getCurrentGlobalCoords()[1];
+		double[] coords = {0,0};
 		
-		// Magically rotate the other point to the x axis around the origin (May not need this)
-		double a = Math.atan2(coords[0], coords[1]);
-		double[] rotCoord = {coords[0] * Math.cos(a) - coords[1] * Math.sin(a), coords[0] * Math.sin(a) + coords[1] * Math.cos(a)};
+		if(systemLayer.getCurrentLocalCoords()[0] == coords[0] && systemLayer.getCurrentLocalCoords()[1] == coords[1]){
+			coords = path.getNextCoordinate();
+		}
 		
-		// Set the speed proportionally to the next coord relative to the origin (e.g (40,50) gives a ratio of 4/5)
-		systemLayer.setDriveLeft(Math.abs(coords[0]/coords[1]));
-		systemLayer.setDriveRight(Math.abs(coords[1]/coords[0]));
+//		// Magically rotate the other point to the x axis around the origin (May not need this)
+//		double a = Math.atan2(coords[0], coords[1]);
+//		double[] rotCoord = {coords[0] * Math.cos(a) - coords[1] * Math.sin(a), coords[0] * Math.sin(a) + coords[1] * Math.cos(a)};
+		
+		/*
+		 * Set the speed proportionally to the next coord relative to the origin (e.g (40,50) gives a ratio of 4/5); skim the extra.
+		 * Since the path's first point is our current position it will be a ratio relative to the origin.
+		 */
+		systemLayer.setDriveLeft(skim(Math.abs(coords[0]/coords[1])));
+		systemLayer.setDriveRight(skim(Math.abs(coords[1]/coords[0])));
 		
 	}
-
+	
+	private double skim(double vel){
+		if(vel > 1)
+			return -((vel - 1.0) * gain);
+		else if(vel < -1)
+			return -((vel + 1.0) * gain);
+		return vel;
+	}
+	
 	@Override
 	public String getName(){
 		return "TELEOP_VISION_TRACK";
