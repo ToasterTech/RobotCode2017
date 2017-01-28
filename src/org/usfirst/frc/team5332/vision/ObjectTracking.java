e9e69c42b6674a678810b5f13a05b14c080e6426
Krishan <Krishan@128.61.56.138>
2017-01-28 16:00:47 -0500

    LocateTarget method

--- src/org/usfirst/frc/team5332/vision/ObjectTracking.java
+++ src/org/usfirst/frc/team5332/vision/ObjectTracking.java
@@ -23,7 +23,7 @@ public class ObjectTracking {
 		return coloredImg;
 	}
 		
-	public static void getContour(Mat coloredImg) {
+	public static ArrayList<MatOfPoint> getContour(Mat coloredImg) {
 		Mat blurredImage = new Mat();
 		Imgproc.blur(coloredImg, blurredImage, new Size(7, 7));
 		List<MatOfPoint> contours = new ArrayList<>();
@@ -43,28 +43,33 @@ public class ObjectTracking {
 				biggestContour2 = contour;
 			}
 		}
-		if (biggestContour1 != null) {
-			MatOfPoint2f biggestContour12f = new MatOfPoint2f();
-			MatOfPoint2f biggestContour22f = new MatOfPoint2f();
+		ArrayList<MatOfPoint> tape = new ArrayList<>();
+		tape.add(biggestContour1);
+		tape.add(biggestContour2);
+		return tape;
+	}
+	
+	public static void locateTarget(Mat uneditedImg, ArrayList<MatOfPoint> tape) {
+		if (uneditedImg != null && tape.get(0) != null && tape.get(1) != null) {
+			MatOfPoint2f tape1 = new MatOfPoint2f();
+			MatOfPoint2f tape2 = new MatOfPoint2f();
 			double xSum = 0;
 			double ySum = 0;
-			contours.remove(biggestContour1);
-			contours.remove(biggestContour2);
-			Imgproc.drawContours(coloredImg, contours, -1, new Scalar(0, 255, 255), -1);
-			Imgproc.approxPolyDP(new MatOfPoint2f(biggestContour1.toArray()), biggestContour12f, 3.0, true);
-			Imgproc.approxPolyDP(new MatOfPoint2f(biggestContour2.toArray()), biggestContour22f, 3.0, true);
-			for (Point center1: biggestContour12f.toArray()) {
+			Imgproc.drawContours(uneditedImg, tape, -1, new Scalar(60, 255, 255), 5);
+			Imgproc.approxPolyDP(new MatOfPoint2f(tape.get(0).toArray()), tape1, 3.0, true);
+			Imgproc.approxPolyDP(new MatOfPoint2f(tape.get(1).toArray()), tape2, 3.0, true);
+			for (Point center1: tape1.toArray()) {
 				xSum += center1.x;
 				ySum += center1.y;
+				
 			}
-			for (Point center2: biggestContour22f.toArray()) {
+			for (Point center2: tape2.toArray()) {
 				xSum += center2.x;
 				ySum += center2.y;
 			}
-			double xCenter = xSum / ((biggestContour12f.toArray().length) + (biggestContour22f.toArray().length));
-			double yCenter = ySum / ((biggestContour12f.toArray().length) + (biggestContour22f.toArray().length));
-			Imgproc.circle(coloredImg, new Point(xCenter, yCenter), 5, new Scalar(0, 255, 255));
-				
+			double xCenter = xSum / ((tape1.toArray().length) + (tape2.toArray().length));
+			double yCenter = ySum / ((tape1.toArray().length) + (tape2.toArray().length));
+			Imgproc.circle(uneditedImg, new Point(xCenter, yCenter), 5, new Scalar(0, 255, 255));	
 		}
 	}
 }
