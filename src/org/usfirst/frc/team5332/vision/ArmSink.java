0d2ffcd698ed668399e67c3e1851de9b2dbd4943
myaccount <myaccount@lawn-143-215-53-45.lawn.gatech.edu>
2017-02-17 20:42:23 -0500

    Created class ArmSink and began work on math stuff inside Systems.

---
+++ src/org/usfirst/frc/team5332/vision/ArmSink.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team5332.vision;
+
+public class ArmSink {
+
+	public static double getDist(){
+		return 0;
+	}
+	public static  double getTheta(){
+		return 0;
+	}
+	public static double getX() {
+		return 0;
+	}
+	public static double getY() {
+		return 0;
+	}
+	
+} 
