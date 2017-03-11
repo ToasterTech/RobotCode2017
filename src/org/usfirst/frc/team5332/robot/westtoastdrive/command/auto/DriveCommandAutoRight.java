2af9726d7056a5450b2136710774eb1792c8bb88
myaccount <myaccount@lawn-143-215-53-84.lawn.gatech.edu>
2017-03-10 21:04:00 -0500

    Started work on autonomous drive for left and right gears.

---
+++ src/org/usfirst/frc/team5332/robot/westtoastdrive/command/auto/DriveCommandAutoRight.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team5332.robot.westtoastdrive.command.auto;
+
+import org.usfirst.frc.team5332.robot.westtoastdrive.base.DriveCommandBase;
+
+public class DriveCommandAutoRight extends DriveCommandBase{
+
+	private double speed;
+	private double seconds;
+	private long startTime;
+	
+	public DriveCommandAutoRight(double speed, double seconds){
+		this.speed = speed;
+		this.seconds = seconds;
+	}
+	
+	@Override
+	public void init() {
+		startTime = System.currentTimeMillis();
+	}
+
+	@Override
+	public void periodicUpdate() {
+		if((System.currentTimeMillis() - startTime)/1000 < seconds){
+			systemLayer.setDriveLeft(speed);
+			systemLayer.setDriveRight(speed);
+		}else{
+			systemLayer.setDriveLeft(0);
+			systemLayer.setDriveRight(0);
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "DRIVE_AUTO_STRAIGHT";
+	}
+
+}
+
+
