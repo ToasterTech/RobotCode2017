dd027f779ba788735a66ecf364950a0d4a307b9b
Miriam <Miriam@128.61.1.54>
2017-03-31 20:27:06 -0400

    updated gearintakehardwarebase

--- src/org/usfirst/frc/team5332/robot/gearintake/base/GearIntakeHardwareBase.java
+++ src/org/usfirst/frc/team5332/robot/gearintake/base/GearIntakeHardwareBase.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc.team5332.robot.gearintake.base;
 
-public class GearIntakeHardwareBase {
+import org.usfirst.frc.team5332.subsystem.BaseLayer;;
 
+public abstract class GearIntakeHardwareBase {
+	
+	public abstract void extendPneumatics();
+	
+	public abstract void retractPneumatics();
+	
+	public abstract void setIntakeSpeed(double speed);
 }
