871588b537e46503a694ab8713dd7ad1337dd730
Miriam <Miriam@128.61.1.54>
2017-04-01 16:10:38 -0400

    updated GearIntakeSystemBase & GearIntakeSystem

--- src/org/usfirst/frc/team5332/robot/gearintake/GearIntakeSystem.java
+++ src/org/usfirst/frc/team5332/robot/gearintake/GearIntakeSystem.java
@@ -1,5 +1,59 @@
 package org.usfirst.frc.team5332.robot.gearintake;
 
-public class GearIntakeSystem {
+import org.usfirst.frc.team5332.robot.gearintake.base.GearIntakeHardwareBase;
+import org.usfirst.frc.team5332.robot.gearintake.base.GearIntakeSystemBase;
 
+public class GearIntakeSystem extends GearIntakeSystemBase {
+ 
+	private GearIntakeHardwareBase hardwareLayer;	
+	
+	public GearIntakeSystem() {
+		hardwareLayer = new GearIntakeHardware();
+	}
+	
+	@Override
+	public void init() {
+		
+	}
+
+	@Override
+	public void periodicUpdate() {
+		
+	}
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public void moveIntakeDown() {
+		hardwareLayer.extendPneumatics();
+	}
+	
+	@Override
+	public void moveIntakeUp() {
+		hardwareLayer.retractPneumatics();
+	}
+
+	@Override
+	public void runForwards() {
+		hardwareLayer.setIntakeSpeed(1);
+	}
+
+	@Override
+	public void runReverse() {
+		hardwareLayer.setIntakeSpeed(-1);
+	}
+
+	@Override
+	public void runStop() {
+		hardwareLayer.setIntakeSpeed(0);
+	}
+
+	@Override
+	public void setChild(GearIntakeHardwareBase c) {
+		hardwareLayer = c;
+	}
+	
 }
--- src/org/usfirst/frc/team5332/robot/gearintake/base/GearIntakeSystemBase.java
+++ src/org/usfirst/frc/team5332/robot/gearintake/base/GearIntakeSystemBase.java
@@ -1,5 +1,16 @@
 package org.usfirst.frc.team5332.robot.gearintake.base;
 
-public class GearIntakeSystemBase {
+import org.usfirst.frc.team5332.subsystem.BaseLayer;
+import org.usfirst.frc.team5332.subsystem.Layer;
 
+public abstract class GearIntakeSystemBase implements Layer<GearIntakeHardwareBase>  {
+	
+	//Gear intake "arm"
+	public abstract void moveIntakeDown();
+	public abstract void moveIntakeUp();
+	
+	//Gear intake wheels
+	public abstract void runForwards();
+	public abstract void runReverse();
+	public abstract void runStop();
 }
